0a1e72f1b7ba045cf3f612afde225bb0860c1522
unknown <jeppen@jampe.dk>
2014-04-03 16:50:51 +0800

    OGTexture clipping

--- OpenGUI/Assets/Plugins/OpenGUI/Scripts/OGTexture.js
+++ OpenGUI/Assets/Plugins/OpenGUI/Scripts/OGTexture.js
@@ -25,25 +25,50 @@ public class OGTexture extends OGWidget {
 			return;
 		}
 
+		var displayRct : Rect = drawRct;
+		var uvRct : Rect = drawCrd;
+
+		if ( clipTo ) {
+			if ( clipTo.drawRct.xMin > displayRct.xMin ) {
+				uvRct.xMin = ( clipTo.drawRct.xMin - displayRct.xMin ) / this.transform.lossyScale.x;
+				displayRct.xMin = clipTo.drawRct.xMin;
+			}
+			
+			if ( clipTo.drawRct.xMax < displayRct.xMax ) {
+				uvRct.xMax = ( displayRct.xMax - clipTo.drawRct.xMax ) / this.transform.lossyScale.x;
+				displayRct.xMax = clipTo.drawRct.xMax;
+			}
+			
+			if ( clipTo.drawRct.yMin > displayRct.yMin ) {
+				uvRct.yMin = ( clipTo.drawRct.yMin - displayRct.yMin ) / this.transform.lossyScale.y;
+				displayRct.yMin = clipTo.drawRct.yMin;
+			}
+			
+			if ( clipTo.drawRct.yMax < displayRct.yMax ) {
+				uvRct.yMax = ( displayRct.yMax - clipTo.drawRct.yMax ) / this.transform.lossyScale.y;
+				displayRct.yMax = clipTo.drawRct.yMax;
+			}
+		}
+
 		GL.Begin(GL.QUADS);
 		
 		material.SetPass ( 0 );
 
 		// Bottom Left	
-		GL.TexCoord2 ( drawCrd.x, drawCrd.y );
-		GL.Vertex3 ( drawRct.x, drawRct.y, drawDepth );
+		GL.TexCoord2 ( uvRct.x, uvRct.y );
+		GL.Vertex3 ( displayRct.x, displayRct.y, drawDepth );
 		
 		// Top left
-		GL.TexCoord2 ( drawCrd.x, drawCrd.y + drawCrd.height );
-		GL.Vertex3 ( drawRct.x, drawRct.y + drawRct.height, drawDepth );
+		GL.TexCoord2 ( uvRct.x, uvRct.y + uvRct.height );
+		GL.Vertex3 ( displayRct.x, displayRct.y + displayRct.height, drawDepth );
 		
 		// Top right
-		GL.TexCoord2 ( drawCrd.x + drawCrd.width, drawCrd.y + drawCrd.height );
-		GL.Vertex3 ( drawRct.x + drawRct.width, drawRct.y + drawRct.height, drawDepth );
+		GL.TexCoord2 ( uvRct.x + uvRct.width, uvRct.y + uvRct.height );
+		GL.Vertex3 ( displayRct.x + displayRct.width, displayRct.y + displayRct.height, drawDepth );
 		
 		// Bottom right
-		GL.TexCoord2 ( drawCrd.x + drawCrd.width, drawCrd.y );
-		GL.Vertex3 ( drawRct.x + drawRct.width, drawRct.y, drawDepth );
+		GL.TexCoord2 ( uvRct.x + uvRct.width, uvRct.y );
+		GL.Vertex3 ( displayRct.x + displayRct.width, displayRct.y, drawDepth );
 		
 		GL.End();
 	}
