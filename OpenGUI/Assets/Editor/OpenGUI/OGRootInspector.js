10a68993bd4357c6dc67506c0f384e9195e7650a
unknown <jeppen@jampe.dk>
2014-04-11 11:27:24 +0800

    improved ongui selection

--- OpenGUI/Assets/Editor/OpenGUI/OGRootInspector.js
+++ OpenGUI/Assets/Editor/OpenGUI/OGRootInspector.js
@@ -4,8 +4,33 @@
 @InitializeOnLoad
 public class OGRootInspector extends Editor {
 	private static function OGRootInspector () {
-		OGRoot.EditorSelectWidget = function ( w : OGWidget ) {
-			Selection.activeObject = w.gameObject;
+		OGRoot.EditorSelectWidget = function ( w : OGWidget, additive : boolean ) {
+			if ( !w ) {
+				Selection.activeObject = null;
+
+			} else if ( additive ) {
+				var list : List.< UnityEngine.Object > = new List.< UnityEngine.Object > ( Selection.objects );
+				var remove : boolean = false;
+
+				for ( var i : int = 0; i < list.Count; i++ ) {
+					if ( list[i] as GameObject == w.gameObject ) {
+						remove = true;
+						break;
+					}
+				}
+				
+				if ( remove ) {
+					list.Remove ( w.gameObject );
+				} else {
+					list.Add ( w.gameObject );
+				}
+
+				Selection.objects = list.ToArray ();
+			
+			} else {
+				Selection.activeObject = w.gameObject;
+			
+			}
 		};
 	}
 	
